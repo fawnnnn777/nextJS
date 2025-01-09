902bc35fa85dce5811f0e20997b406164bcf3048
Diego <ale.rgdap@gmail.com>
2025-01-09 08:28:18 -0600

    fix font

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -3,5 +3,4 @@ import { Montserrat } from 'next/font/google'
 import { Lusitana } from 'next/font/google'
 
 export const inter = Inter({subsets: ['latin']})
-export const monstserrat = Montserrat({weight: ['500']})
 export const lusitana = Lusitana({weight: ['700']})
